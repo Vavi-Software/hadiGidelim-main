1c4ce6ecb9a982a44c24162db26dca96cd883e0a
DKadir13 <deveci.kadir@outlook.com>
2024-11-25 01:35:41 +0300

    ilceler eklendi

--- backend/src/components/AdminPanel/AddBusinessForm.tsx
+++ backend/src/components/AdminPanel/AddBusinessForm.tsx
@@ -10,6 +10,30 @@ import {
   FormControl,
 } from "@mui/material";
 
+const districtData: Record<string, string[]> = {
+  Akyurt: ['Ahmetadil', 'Balıkhisar', 'Çalışkanlar', 'Çardakbağı', 'Yeşiltepe', 'Yıldırım'],
+  Altındağ: ['Aydınlıkevler', 'Battalgazi', 'Beşikkaya', 'Bentderesi', 'Doğantepe', 'Hacettepe', 'Hamamönü', 'Karapürçek', 'Kale Mahallesi', 'Siteler', 'Ulus', 'Zübeyde Hanım'],
+  Bala: ['Afşar', 'Bahçekaralı', 'Karahamzalı', 'Kesikköprü', 'Sarıhüyük', 'Yeniköy'],
+  Beypazarı: ['Cumhuriyet', 'Fatih', 'İnözü', 'Karaşar', 'Yeni Mahalle', 'Zafer'],
+  Çankaya: ['Ayrancı', 'Bahçelievler', 'Beytepe', 'Bilkent', 'Cebeci', 'Çukurambar', 'Dikmen', 'Gazi Osman Paşa', 'İncesu', 'Kızılay', 'Maltepe', 'Oran', 'Ümitköy', 'Yıldızevler'],
+  Çubuk: ['Atatürk', 'Barbaros', 'Cumhuriyet', 'Kızılören', 'Tahtayazı', 'Yıldırım Beyazıt'],
+  Elmadağ: ['Hasanoğlan', 'İstasyon', 'Kayadibi', 'Şentepe', 'Tatlıca', 'Yeni Mahalle'],
+  Etimesgut: ['Ahi Mesut', 'Alsancak', 'Bağlıca', 'Elvankent', 'Eryaman', 'Göksu', 'Topçu Mahallesi', 'Turan Güneş', 'Yeşilova'],
+  Evren: ['Bahçelievler', 'Çatalören', 'Kale', 'Yeni Mahalle'],
+  Gölbaşı: ['Bahçelievler', 'Gazi Mahallesi', 'Hacılar', 'İncek', 'Karşıyaka', 'Karaoğlan', 'Tulumtaş'],
+  Haymana: ['Bahçelievler', 'Cuma Mahallesi', 'Karakaya', 'Medrese', 'Yamak', 'Yenimahalle'],
+  Kalecik: ['Kurtuluş', 'Ulucanlar', 'Yeni Mahalle', 'Yenişehir', 'Yenimahalle'],
+  Kahramankazan: ['Akıncı', 'Fatih', 'Kayı', 'Saray', 'Yıldırım'],
+  Keçiören: ['Aktepe', 'Atapark', 'Ayvalı', 'Esertepe', 'Etlik', 'Gazino', 'Kalaba', 'Kuşcağız', 'Pınarbaşı', 'Şefkat', 'Ufuktepe', 'Yayla'],
+  Kızılcahamam: ['Bağlıca', 'Çağlayan', 'Güvem', 'Saray', 'Yenice'],
+  Mamak: ['Akdere', 'Durali Alıç', 'Ege Mahallesi', 'General Zeki Doğan', 'Gülveren', 'Karaağaç', 'Kutlu', 'Mutlu', 'Saimekadın', 'Şahintepe', 'Tuzluçayır'],
+  Nallıhan: ['Çayırhan', 'Uluhan'],
+  Polatlı: ['Cumhuriyet', 'Fatih', 'Gazi Mahallesi', 'Şentepe', 'Sakarya', 'Yeni Mahalle', 'Zafer'],
+  Pursaklar: ['Altınova', 'Karacaören', 'Merkez Mahallesi', 'Mimar Sinan', 'Saray', 'Tevfik İleri'],
+  Sincan: ['Akşemsettin', 'Atatürk Mahallesi', 'Fatih', 'Mevlana', 'Osmaniye', 'Pınarbaşı', 'Törekent', 'Ulubatlı', 'Yenikent'],
+  Yenimahalle: ['Batıkent', 'Demetevler', 'Gazi Mahallesi', 'İvedik', 'Karşıyaka', 'Macunköy', 'Ostim', 'Ragıp Tüzün', 'Şentepe', 'Yeniçağ', 'Yuva']
+
+};
 const BusinessForm: React.FC = () => {
   const [businessType, setBusinessType] = useState<string>("");
   const [businessName, setBusinessName] = useState<string>("");
@@ -79,7 +103,7 @@ const BusinessForm: React.FC = () => {
       />
   
   <FormControl fullWidth sx={{ marginBottom: "20px" }}>
-        <InputLabel id="business-type-label">İşletme Türü</InputLabel>
+        <InputLabel id="business-type-label">İşletme İlçesi</InputLabel>
         <Select
           labelId="business-type-label"
           value={city}
